f2dfe891b8e3b59fe7e8231b09a73fe845871815
Gunj Desai <gunjdesai@gmail.com>
2021-12-06 12:25:13 +0530

    Components Update

--- helpers/utils.go
+++ helpers/utils.go
@@ -1,6 +1,18 @@
 package helpers
 
-import "os"
+import (
+	"os"
+	"path"
+	"path/filepath"
+	"runtime"
+	"time"
+)
+
+func GetRootDir() string {
+	_, b, _, _ := runtime.Caller(0)
+	d := path.Join(path.Dir(b))
+	return filepath.Dir(d)
+}
 
 func GetEnv() string {
 
@@ -20,5 +32,20 @@ func GetEnv() string {
 	}
 
 	return env
+
+}
+
+func DatesAreEqual(t1 time.Time, t2 time.Time) bool {
+
+	return t1.Truncate(24 * time.Hour).Equal(t2.Truncate(24 * time.Hour))
+
+}
+
+func DatesLiesWithinRange(t time.Time, lb time.Time, ub time.Time) bool {
+
+	return lb.Truncate(24*time.Hour).After(t.Truncate(24*time.Hour)) &&
+		ub.Truncate(24*time.Hour).Before(t.Truncate(24*time.Hour)) ||
+		t.Truncate(24*time.Hour).Equal(lb.Truncate(24*time.Hour)) ||
+		t.Truncate(24*time.Hour).Equal(ub.Truncate(24*time.Hour))
 
 }
